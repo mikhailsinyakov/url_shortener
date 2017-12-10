32f20abaf5d91854ab61cec05c3ea087ec9532e8
Glitch (raspy-fright) <none>
2017-12-10 12:41:15 +0000

    Init commit

--- server.js
+++ server.js
@@ -1,30 +1,16 @@
 const express = require('express');
 const app = express();
+const port = process.env.PORT;
 app.use(express.static('public'));
 
-// http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (request, response) {
+app.get("/", (request, response) => {
   response.sendFile(__dirname + '/views/index.html');
 });
 
-app.get("/dreams", function (request, response) {
-  response.send(dreams);
+app.post("/dreams", (req, res) => {
+  res.sendStatus(200);
 });
 
-// could also use the POST body instead of query string: http://expressjs.com/en/api.html#req.body
-app.post("/dreams", function (request, response) {
-  dreams.push(request.query.dream);
-  response.sendStatus(200);
-});
-
-// Simple in-memory store for now
-var dreams = [
-  "Find and count some sheep",
-  "Climb a really tall mountain",
-  "Wash the dishes"
-];
-
-// listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+app.listen(port, () => {
+  console.log('Your app is listening on port ' + port);
 });
