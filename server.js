f241f5dcdd17fd048cc98812dfb29f7ff250ba1c
Glitch (raspy-fright) <none>
2017-12-10 13:06:06 +0000

    ☃️👂 Checkpoint server.js:586560

--- server.js
+++ server.js
@@ -3,18 +3,23 @@ const app = express();
 const port = process.env.PORT;
 app.use(express.static('public'));
 
-
-
-app.route("/new/:url")
-    .all((req, res) => {
-      const url = req.params.url;
-      console.log(req.params.url);
-      res.send("H");
-    });
-
 app.get("/", (req, res) => {
   res.sendFile(__dirname + '/views/index.html');
 });
+
+
+app.route("/new/:url")
+    .post((req, res) => {
+      console.log(url);
+      const url = req.params.url;
+      const obj = {
+        original_url: url,
+        short_url: ""
+      };
+      res.set("Content-Type", "application/json");
+      res.json(obj);
+    });
+
 
 app.listen(port, () => {
   console.log('Your app is listening on port ' + port);
