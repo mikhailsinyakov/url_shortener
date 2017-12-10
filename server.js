1217c00e4657d51f060e05876683b1b6b9a7328e
Glitch (raspy-fright) <none>
2017-12-10 16:24:05 +0000

    🐺🔦 Checkpoint server.js:586560 package.json:586560

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT;
+const db = require("./database");
+
 app.use(express.static('public'));
 
 app.get("/", (req, res) => {
@@ -10,13 +12,14 @@ app.get("/", (req, res) => {
 
 app.get(/\/new\/[\S]+/, (req, res) => {
       const query = req.url.slice(5);
-      console.log(query)
       let obj = {};
   
       // If a query is a valid url
       if (query.match(/https?:\/\/(www.)?[\w]+.[\w]{2,5}(\/[\w\?=&-]+)*/)) {
-        console.log(query.match(/https?:\/{2}(www.)?[\w]+.[\w]{2,5}(\/[\w\?=&-]+)*/))
         const url = query;
+        
+        
+        
         obj = {
           original_url: url,
           short_url: ""
@@ -24,8 +27,8 @@ app.get(/\/new\/[\S]+/, (req, res) => {
       }
       else {
         obj = {
-        error: "Wrong url format, make sure you have a valid protocol and real site."
-      };
+          error: "Wrong url format, make sure you have a valid protocol and real site."
+        };
       }
       
       res.set("Content-Type", "application/json");
